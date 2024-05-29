d1918dcba6fa543b0945308acd4f6288cef80bdb
Gustavo Cavalcante Gomes <133007805+ygustx@users.noreply.github.com>
2024-05-29 14:09:37 -0300

    .

--- Projeto/cad_user.php
+++ Projeto/cad_user.php
@@ -22,7 +22,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="refresh" 
-    content="3,http://localhost/Projeto/">
+    content="3,http://localhost/Projeto/pag_inicial.html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="estilo.css">
     <title>Confirmação de Cadastro</title>
--- Projeto/conexao.php
+++ Projeto/conexao.php
@@ -12,7 +12,7 @@ DBNAME - Nome do banco de dados
 $servidor = "localhost";
 $usuario = "root";
 $senha = "";
-$dbname = "biblioteca";
+$dbname = "bd_remix";
 
 //Criar conexao
 
